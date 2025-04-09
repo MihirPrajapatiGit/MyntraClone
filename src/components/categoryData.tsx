8cec456ccd309df52031de539c9a2e8f53c0edb6
MihirPrajapatiGit <prajapatimihir502@gmail.com>
2025-04-09 16:17:40 +0530

    remove public 3

--- src/components/categoryData.tsx
+++ src/components/categoryData.tsx
@@ -1,55 +1,55 @@
 const categoryImages1: string[] = [
-  "../../../public/shopbycategory/1.jpg",
-  "../../../public/shopbycategory/2.jpg",
-  "../../../public/shopbycategory/3.jpg",
-  "../../../public/shopbycategory/4.jpg",
-  "../../../public/shopbycategory/5.jpg",
-  "../../../public/shopbycategory/6.jpg",
+  "/shopbycategory/1.jpg",
+  "/shopbycategory/2.jpg",
+  "/shopbycategory/3.jpg",
+  "/shopbycategory/4.jpg",
+  "/shopbycategory/5.jpg",
+  "/shopbycategory/6.jpg",
 ];
 
 const categoryImages2: string[] = [
-  "../../../public/shopbycategory/10007.jpg",
-  "../../../public/shopbycategory/10008.jpg",
-  "../../../public/shopbycategory/10009.jpg",
-  "../../../public/shopbycategory/10010.jpg",
-  "../../../public/shopbycategory/10011.jpg",
-  "../../../public/shopbycategory/10012.jpg",
+  "/shopbycategory/10007.jpg",
+  "/shopbycategory/10008.jpg",
+  "/shopbycategory/10009.jpg",
+  "/shopbycategory/10010.jpg",
+  "/shopbycategory/10011.jpg",
+  "/shopbycategory/10012.jpg",
 ];
 
 const categoryImages3: string[] = [
-  "../../../public/shopbycategory/10013.jpg",
-  "../../../public/shopbycategory/10014.jpg",
-  "../../../public/shopbycategory/10015.jpg",
-  "../../../public/shopbycategory/10016.jpg",
-  "../../../public/shopbycategory/10017.jpg",
-  "../../../public/shopbycategory/10018.jpg",
+  "/shopbycategory/10013.jpg",
+  "/shopbycategory/10014.jpg",
+  "/shopbycategory/10015.jpg",
+  "/shopbycategory/10016.jpg",
+  "/shopbycategory/10017.jpg",
+  "/shopbycategory/10018.jpg",
 ];
 
 const categoryImages4: string[] = [
-  "../../../public/shopbycategory/10019.jpg",
-  "../../../public/shopbycategory/10020.jpg",
-  "../../../public/shopbycategory/10021.jpg",
-  "../../../public/shopbycategory/10022.jpg",
-  "../../../public/shopbycategory/10023.jpg",
-  "../../../public/shopbycategory/10024.jpg",
+  "/shopbycategory/10019.jpg",
+  "/shopbycategory/10020.jpg",
+  "/shopbycategory/10021.jpg",
+  "/shopbycategory/10022.jpg",
+  "/shopbycategory/10023.jpg",
+  "/shopbycategory/10024.jpg",
 ];
 
 const categoryImages5: string[] = [
-  "../../../public/shopbycategory/10025.jpg",
-  "../../../public/shopbycategory/10026.jpg",
-  "../../../public/shopbycategory/10027.jpg",
-  "../../../public/shopbycategory/10028.jpg",
-  "../../../public/shopbycategory/10029.jpg",
-  "../../../public/shopbycategory/10030.jpg",
+  "/shopbycategory/10025.jpg",
+  "/shopbycategory/10026.jpg",
+  "/shopbycategory/10027.jpg",
+  "/shopbycategory/10028.jpg",
+  "/shopbycategory/10029.jpg",
+  "/shopbycategory/10030.jpg",
 ];
 
 const categoryImages6: string[] = [
-  "../../../public/shopbycategory/10031.jpg",
-  "../../../public/shopbycategory/10032.jpg",
-  "../../../public/shopbycategory/10033.jpg",
-  "../../../public/shopbycategory/10034.jpg",
-  "../../../public/shopbycategory/10035.jpg",
-  "../../../public/shopbycategory/10036.jpg",
+  "/shopbycategory/10031.jpg",
+  "/shopbycategory/10032.jpg",
+  "/shopbycategory/10033.jpg",
+  "/shopbycategory/10034.jpg",
+  "/shopbycategory/10035.jpg",
+  "/shopbycategory/10036.jpg",
 ];
 
 export {
