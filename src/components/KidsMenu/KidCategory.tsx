4dd6aa9e443204b2da55e5945f479e0bb0a69feb
MihirPrajapatiGit <prajapatimihir502@gmail.com>
2025-04-16 13:22:35 +0530

    change kid img path

--- src/components/KidsMenu/KidCategory.tsx
+++ src/components/KidsMenu/KidCategory.tsx
@@ -10,15 +10,15 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="favourite-brands">
-          <img src="../../../public/KIDS-Menu/10001.jpg" />
-          <img src="../../../public/KIDS-Menu/10002.jpg" />
-          <img src="../../../public/KIDS-Menu/10003.jpg" />
-          <img src="../../../public/KIDS-Menu/10004.jpg" />
-          <img src="../../../public/KIDS-Menu/10005.jpg" />
-          <img src="../../../public/KIDS-Menu/10006.jpg" />
-          <img src="../../../public/KIDS-Menu/10007.jpg" />
-          <img src="../../../public/KIDS-Menu/10008.jpg" />
-          <img src="../../../public/KIDS-Menu/10009.jpg" />
+          <img src="/KIDS-Menu/10001.jpg" />
+          <img src="/KIDS-Menu/10002.jpg" />
+          <img src="/KIDS-Menu/10003.jpg" />
+          <img src="/KIDS-Menu/10004.jpg" />
+          <img src="/KIDS-Menu/10005.jpg" />
+          <img src="/KIDS-Menu/10006.jpg" />
+          <img src="/KIDS-Menu/10007.jpg" />
+          <img src="/KIDS-Menu/10008.jpg" />
+          <img src="/KIDS-Menu/10009.jpg" />
         </section>
       </Link>
       <div className="heading_div">
@@ -26,17 +26,17 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="top-picks">
-          <img src="../../../public/KIDS-MENU/10010.jpg" />
-          <img src="../../../public/KIDS-MENU/10011.jpg" />
-          <img src="../../../public/KIDS-MENU/10012.jpg" />
-          <img src="../../../public/KIDS-MENU/10013.jpg" />
-          <img src="../../../public/KIDS-MENU/10014.jpg" />
-          <img src="../../../public/KIDS-MENU/10015.jpg" />
-          <img src="../../../public/KIDS-MENU/10016.jpg" />
-          <img src="../../../public/KIDS-MENU/10017.jpg" />
-          <img src="../../../public/KIDS-MENU/10018.jpg" />
-          <img src="../../../public/KIDS-MENU/10019.jpg" />
-          <img src="../../../public/KIDS-MENU/10020.jpg" />
+          <img src="/KIDS-MENU/10010.jpg" />
+          <img src="/KIDS-MENU/10011.jpg" />
+          <img src="/KIDS-MENU/10012.jpg" />
+          <img src="/KIDS-MENU/10013.jpg" />
+          <img src="/KIDS-MENU/10014.jpg" />
+          <img src="/KIDS-MENU/10015.jpg" />
+          <img src="/KIDS-MENU/10016.jpg" />
+          <img src="/KIDS-MENU/10017.jpg" />
+          <img src="/KIDS-MENU/10018.jpg" />
+          <img src="/KIDS-MENU/10019.jpg" />
+          <img src="/KIDS-MENU/10020.jpg" />
         </section>
       </Link>
       <div className="heading_div">
@@ -44,11 +44,11 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="iconic-brands">
-          <img src="../../../public/KIDS-MENU/10021.jpg" />
-          <img src="../../../public/KIDS-MENU/10022.jpg" />
-          <img src="../../../public/KIDS-MENU/10023.jpg" />
-          <img src="../../../public/KIDS-MENU/10024.jpg" />
-          <img src="../../../public/KIDS-MENU/10025.jpg" />
+          <img src="/KIDS-MENU/10021.jpg" />
+          <img src="/KIDS-MENU/10022.jpg" />
+          <img src="/KIDS-MENU/10023.jpg" />
+          <img src="/KIDS-MENU/10024.jpg" />
+          <img src="/KIDS-MENU/10025.jpg" />
         </section>
       </Link>
       <div className="heading_div">
@@ -56,18 +56,18 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="fashion-essentials">
-          <img src="../../../public/KIDS-MENU/10026.jpg" />
-          <img src="../../../public/KIDS-MENU/10027.jpg" />
-          <img src="../../../public/KIDS-MENU/10028.jpg" />
-          <img src="../../../public/KIDS-MENU/10029.jpg" />
-          <img src="../../../public/KIDS-MENU/10030.jpg" />
+          <img src="/KIDS-MENU/10026.jpg" />
+          <img src="/KIDS-MENU/10027.jpg" />
+          <img src="/KIDS-MENU/10028.jpg" />
+          <img src="/KIDS-MENU/10029.jpg" />
+          <img src="/KIDS-MENU/10030.jpg" />
         </section>
         <section className="fashion-essentials">
-          <img src="../../../public/KIDS-MENU/10031.jpg" />
-          <img src="../../../public/KIDS-MENU/10032.jpg" />
-          <img src="../../../public/KIDS-MENU/10033.jpg" />
-          <img src="../../../public/KIDS-MENU/10034.jpg" />
-          <img src="../../../public/KIDS-MENU/10035.jpg" />
+          <img src="/KIDS-MENU/10031.jpg" />
+          <img src="/KIDS-MENU/10032.jpg" />
+          <img src="/KIDS-MENU/10033.jpg" />
+          <img src="/KIDS-MENU/10034.jpg" />
+          <img src="/KIDS-MENU/10035.jpg" />
         </section>
       </Link>
       <div className="heading_div">
@@ -75,16 +75,16 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="explore-more">
-          <img src="../../../public/KIDS-MENU/10036.jpg" />
-          <img src="../../../public/KIDS-MENU/10037.jpg" />
+          <img src="/KIDS-MENU/10036.jpg" />
+          <img src="/KIDS-MENU/10037.jpg" />
         </section>
         <section className="explore-more">
-          <img src="../../../public/KIDS-MENU/10038.jpg" />
-          <img src="../../../public/KIDS-MENU/10039.jpg" />
+          <img src="/KIDS-MENU/10038.jpg" />
+          <img src="/KIDS-MENU/10039.jpg" />
         </section>
         <section className="explore-more">
-          <img src="../../../public/KIDS-MENU/10040.jpg" />
-          <img src="../../../public/KIDS-MENU/10041.jpg" />
+          <img src="/KIDS-MENU/10040.jpg" />
+          <img src="/KIDS-MENU/10041.jpg" />
         </section>
       </Link>
       <div className="heading_div">
@@ -92,8 +92,8 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="kids-space">
-          <img src="../../../public/KIDS-MENU/10042.jpg" />
-          <img src="../../../public/KIDS-MENU/10043.jpg" />
+          <img src="/KIDS-MENU/10042.jpg" />
+          <img src="/KIDS-MENU/10043.jpg" />
         </section>
       </Link>
       <div className="heading_div">
@@ -101,12 +101,12 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="budget-buy">
-          <img src="../../../public/KIDS-MENU/10044.jpg" />
-          <img src="../../../public/KIDS-MENU/10045.jpg" />
-          <img src="../../../public/KIDS-MENU/10046.jpg" />
-          <img src="../../../public/KIDS-MENU/10047.jpg" />
-          <img src="../../../public/KIDS-MENU/10048.jpg" />
-          <img src="../../../public/KIDS-MENU/10049.jpg" />
+          <img src="/KIDS-MENU/10044.jpg" />
+          <img src="/KIDS-MENU/10045.jpg" />
+          <img src="/KIDS-MENU/10046.jpg" />
+          <img src="/KIDS-MENU/10047.jpg" />
+          <img src="/KIDS-MENU/10048.jpg" />
+          <img src="/KIDS-MENU/10049.jpg" />
         </section>
       </Link>
       <div className="heading_div">
@@ -114,11 +114,11 @@ const Main = () => {
       </div>
       <Link to="/KIDS-Menu/products">
         <section className="shop-more-brands">
-          <img src="../../../public/KIDS-MENU/10050.jpg" />
-          <img src="../../../public/KIDS-MENU/10051.jpg" />
-          <img src="../../../public/KIDS-MENU/10052.jpg" />
-          <img src="../../../public/KIDS-MENU/10053.jpg" />
-          <img src="../../../public/KIDS-MENU/10054.jpg" />
+          <img src="/KIDS-MENU/10050.jpg" />
+          <img src="/KIDS-MENU/10051.jpg" />
+          <img src="/KIDS-MENU/10052.jpg" />
+          <img src="/KIDS-MENU/10053.jpg" />
+          <img src="/KIDS-MENU/10054.jpg" />
         </section>
       </Link>
     </main>
